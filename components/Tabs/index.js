07633af34b25aa1f4a8391d7c08a09eb9f85cf18
Ruth Poliakon <r.poliakon@gmail.com>
2019-08-18 17:44:55 -0600

    tabs components created

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,28 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+function topicCreator(topicText) {
+  const tab = document.createElement("div");
+  tab.classList.add("tab");
+  tab.textContent = topicText;
+
+  return tab;
+}
+
+const tabsContainer = document.querySelector(".topics");
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(response => {
+    // network request finished
+    const topicsArray = response.data.topics;
+    topicsArray.forEach(item => {
+      const newTab = topicCreator(item);
+      tabsContainer.appendChild(newTab);
+    });
+  })
+  .catch(error => {
+    console.log("Network request was unsuccessful");
+    console.log(error);
+  });
